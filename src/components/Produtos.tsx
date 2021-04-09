24a1363eaf0f5afbdb0f646002c6bc51b5cce61d
crlne <carol.lages@outlook.com>
2021-04-09 00:10:04 -0300

    products images, payment page

--- src/components/Produtos.tsx
+++ src/components/Produtos.tsx
@@ -2,31 +2,62 @@ import { Box, Button, Image, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Produtos() {
     return (
-        <SimpleGrid ml="50" minChildWidth="320px">
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10} flexDirection="column">
-          <Image m="auto" mt="-20" align="center" src="https://cdn.dribbble.com/users/2214836/screenshots/14804408/media/a09bdea30c37c5c496067e6e81af1d1e.png?compress=1&resize=800x600"></Image>
+        <SimpleGrid maxWidth={1480} ml="50" flexDirection="column" minChildWidth="400px" px="8">
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97431.png"></Image>
           
             <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
             <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
         </Box>
 
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10} flexDirection="column">
-          <Image m="auto" mt="-20" align="center" src="https://cdn.dribbble.com/users/2214836/screenshots/14804408/media/a09bdea30c37c5c496067e6e81af1d1e.png?compress=1&resize=800x600"></Image>
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center"h="260" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97396.png"></Image>
           
             <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
             <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
         </Box>
 
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10} flexDirection="column">
-          <Image m="auto" mt="-20" align="center" src="https://cdn.dribbble.com/users/2214836/screenshots/14804408/media/a09bdea30c37c5c496067e6e81af1d1e.png?compress=1&resize=800x600"></Image>
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center"  src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16887.png"></Image>
           
             <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
             <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
         </Box>
 
+
+
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16824.png"></Image>
+          
+            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
+            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+        </Box>
+
+
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/cappuccino/cappuccino_PNG37.png"></Image>
+          
+            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
+            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+        </Box>
+
+
+        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16856.png"></Image>
+          
+            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
+            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+        </Box>
+
+
+        
+        
       </SimpleGrid>
     );
 
---
+++ src/pages/payment.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Payment() {
+    return (
+        <Flex direction="column" h="100vh">
+            <Text fontSize="3xl" fontWeight="bold" >Nome do Produto</Text>
+
+
+        </Flex>
+    );
+}
