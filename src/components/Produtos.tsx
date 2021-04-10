0740b1e85eb1395d70acf81d639835b5758c0d87
crlne <carol.lages@outlook.com>
2021-04-10 17:59:21 -0300

    produtos page

--- src/components/Produtos.tsx
+++ src/components/Produtos.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import Quantity from "./quantity";
 
 
 
@@ -7,15 +8,19 @@ export default function Produtos() {
  
 
     return (
-        <SimpleGrid maxWidth={1480} ml="50" flexDirection="column" minChildWidth="400px" px="8">
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97431.png"></Image>
+        <SimpleGrid maxWidth={1480} ml="50" flexDirection="column" minChildWidth="400px" px="8" mt="-200">
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97431.png"></Image>
           
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+          <Text fontSize="1rem">Double Expresso</Text>   
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-     
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
+
            <Link href="/payment">
-           <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100"
+           <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10"
             >
               Comprar
             </Button> 
@@ -23,48 +28,69 @@ export default function Produtos() {
         </Box>
         
 
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center"h="260" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97396.png"></Image>
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG97396.png"></Image>
           
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+          <Text fontSize="1rem">Double Expresso</Text>   
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
+            <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10">Comprar</Button>   
         </Box>
 
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center"  src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16887.png"></Image>
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16887.png"></Image>
           
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+            <Text fontSize="1rem">Double Expresso</Text>   
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
-        </Box>
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
 
-
-
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16824.png"></Image>
-          
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
-            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
-        </Box>
-
-
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/cappuccino/cappuccino_PNG37.png"></Image>
-          
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
-            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+            <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10">Comprar</Button>   
         </Box>
 
 
-        <Box mt="200" p="8" bg="yellow.100" w="400px" height="300" borderRadius={10}>
-          <Image m="auto" mt="-20" align="center" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16856.png"></Image>
+
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16824.png"></Image>
           
-            <Text fontSize="1rem">Double Expresso R$ 30.00</Text>
+           <Text fontSize="1rem">Double Expresso</Text>   
             <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
-            <Button colorScheme="yellow" variant="outline" align="center" m="auto" ml="100">Comprar</Button>   
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
+            <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10">Comprar</Button>   
+        </Box>
+
+
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/cappuccino/cappuccino_PNG37.png"></Image>
+          
+          <Text fontSize="1rem">Double Expresso</Text>   
+            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
+            <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10">Comprar</Button>   
+        </Box>
+
+
+        <Box mt="200" p="8" bg="white" w="376px" height="462" borderRadius={10}>
+          <Image m="auto" mt="-20" align="center" h="246px" w="242" src="http://pngimg.com/uploads/mug_coffee/mug_coffee_PNG16856.png"></Image>
+          
+          <Text fontSize="1rem">Double Expresso</Text>   
+            <h1>Lorem ipsum dolor iterable, consectetur adip</h1>
+            <Text fontSize="18" fontWeight="bold" ml="150"> 16,58</Text>
+            <Heading as="h6" mt="-5" size="xs" ml="130">
+             R$
+            </Heading>
+            <Button colorScheme="orange" w="208px" h="50" borderRadius={50} m="auto" align="center" ml="50" mt="10">Comprar</Button>   
         </Box>
 
 
